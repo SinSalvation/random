3b1b62d42ca66dccda92fabac6b29b6d62629eea
SinSalvation <346750538@qq.com>
2017-01-24 16:21:28 +0800

    集成mybatis log4j

--- src/main/java/org/randomheroes/websockets/Connect.java
+++ src/main/java/org/randomheroes/websockets/Connect.java
@@ -2,6 +2,7 @@ package org.randomheroes.websockets;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.log4j.Logger;
 import org.randomheroes.bean.Response;
 import org.randomheroes.bean.User;
 import org.springframework.web.socket.CloseStatus;
@@ -10,18 +11,23 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class Connect extends TextWebSocketHandler {
+    private Logger loginLog = Logger.getLogger("login");
+    private Logger connectLog = Logger.getLogger("connect");
+    private Logger logoutLog = Logger.getLogger("logout");
+    private Logger errorLog = Logger.getLogger("error");
+
     //接收到客户端消息时调用
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) {
         try{
-            System.out.println("session message: " + message.getPayload());
+            loginLog.info("session message: " + message.getPayload());
             JSONObject accept = JSON.parseObject(message.getPayload());
             switch (accept.get("method").toString()){
                 case "login" : {
                     User user = JSONObject.toJavaObject((JSON) accept.get("user"), User.class);
                     Response<User> json = new Response<>();
                     if(user.getUsername().equals("111")){
-                        System.out.println("session id: " + session.getId());   //id与帐号存redis
+                        loginLog.info("session id: " + session.getId());   //id与帐号存redis
                         user .setUid("1");
                         json.setMethod("login");
                         json.setCode("200");
@@ -37,45 +43,39 @@ public class Connect extends TextWebSocketHandler {
                 }
             }
         } catch (Exception e){
-            System.out.println("登陆错误: "+e.getMessage());
+            loginLog.info("login error: " + e.getMessage());
         }
     }
 
     // 与客户端完成连接后调用
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
-//        System.out.println("afterConnectionEstablished");
-//        System.out.println("getId:" + session.getId());
-//        System.out.println("getLocalAddress:" + session.getLocalAddress().toString());
-//        System.out.println("getTextMessageSizeLimit:" + session.getTextMessageSizeLimit());
-//        System.out.println("getUri:" + session.getUri().toString());
+        connectLog.info("getId:" + session.getId());
+        connectLog.info("getLocalAddress:" + session.getLocalAddress());
         try {
             Response json = new Response<>();
             json.setMethod("connect");
             json.setCode("200");
             session.sendMessage(new TextMessage(JSON.toJSONString(json)));
         } catch (Exception e){
-            System.out.println("连接错误:" +e.getMessage());
+            connectLog.info("connect error:" + e.getMessage());
         }
     }
 
     // 消息传输出错时调用
     @Override
-    public void handleTransportError(WebSocketSession session,
-                                     Throwable exception) throws Exception {
-        System.out.println("handleTransportError");
+    public void handleTransportError(WebSocketSession session, Throwable exception) {
+        errorLog.info("error: session id: " + session.getId() + "exception: " + exception.getMessage());
     }
 
     // 一个客户端连接断开时关闭
     @Override
-    public void afterConnectionClosed(WebSocketSession session,
-                                      CloseStatus closeStatus) throws Exception {
-        System.out.println("afterConnectionClosed");
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) {
+        logoutLog.info("session id: " + session.getId());
     }
 
     @Override
     public boolean supportsPartialMessages() {
-        // TODO Auto-generated method stub
         return false;
     }
 }
